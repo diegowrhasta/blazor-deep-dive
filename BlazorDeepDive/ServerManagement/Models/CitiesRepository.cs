5a5c7236c57c158bed9a38febd98205edd43a8f4
Diego Balderrama <diegowrhasta@gmail.com>
2025-09-04 20:42:43 +0900

    Initial Commit

---
+++ BlazorDeepDive/ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,8 @@
+namespace ServerManagement.Models;
+
+public static class CitiesRepository
+{
+    private static readonly List<string> _cities = ["Toronto", "Montreal", "Otawa", "Calgary", "Halifax"];
+
+    public static List<string> Cities => _cities; 
+}
---
+++ BlazorDeepDive/ServerManagement/Models/Server.cs
@@ -0,0 +1,16 @@
+namespace ServerManagement.Models;
+
+public class Server
+{
+    public Server()
+    {
+        var random = new Random();
+        var randomNumber = random.Next(0, 2);
+        IsOnline = randomNumber % 2 == 0;
+    }
+    
+    public int Id { get; set; }
+    public bool IsOnline { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+}
