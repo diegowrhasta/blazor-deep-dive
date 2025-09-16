759337be0ddc7c5bfa21b3071ea24a130406178a
Diego Balderrama <diegowrhasta@gmail.com>
2025-09-16 15:00:09 +0900

    Finishes up to-do list requirements, starts with breaking down of task list component plus temporary workaround code

--- BlazorDeepDive/ServerManagement/Models/TodoItem.cs
+++ BlazorDeepDive/ServerManagement/Models/TodoItem.cs
@@ -4,7 +4,26 @@ public class TodoItem
 {
     public int Id { get; set; }
     public string Text { get; set; } = string.Empty;
-    public bool IsComplete { get; set; }
-    public DateTime CompletedDate { get; set; }
+
+    private bool _isComplete;
+    public bool IsComplete
+    {
+        get => _isComplete;
+        set
+        {
+            if (value)
+            {
+                CompletedDate = DateTime.Now;
+            }
+            else
+            {
+                CompletedDate = null;
+            }
+
+            _isComplete = value;
+        }
+    }
+
+    public DateTime? CompletedDate { get; set; }
     public bool FreshAdd { get; set; }
 };
