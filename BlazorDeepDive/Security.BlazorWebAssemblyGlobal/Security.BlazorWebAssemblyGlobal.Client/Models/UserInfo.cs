8895afd56057ad30d89576934e0b008527c986e1
Diego Balderrama <diegowrhasta@gmail.com>
2025-10-12 10:19:05 +0900

    Adds demo projects and additions to deep dive of auth across different patterns

---
+++ BlazorDeepDive/Security.BlazorWebAssemblyGlobal/Security.BlazorWebAssemblyGlobal.Client/Models/UserInfo.cs
@@ -0,0 +1,8 @@
+namespace Security.BlazorWebAssemblyGlobal;
+
+public class UserInfo
+{
+    public string? UserId { get; set; }
+    public string? Email { get; set; }
+    public string? Role { get; set; }
+}
